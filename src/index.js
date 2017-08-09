64abfdd4322211018eeb177ec66b7682471f20b5
Brian Neisler <brian@airbug.com>
2017-08-09 14:58:39 -0700

    Bump version to v0.1.9. Fix lint issues.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 require('babel-polyfill');
-require("babel-register")({
+require('babel-register')({
   ignore: false,
   babelrc: false,
   presets: [
-    "stage-0",
-    "stage-1",
-    "stage-2",
-    "stage-3"
-  ]
+    'stage-0',
+    'stage-1',
+    'stage-2',
+    'stage-3',
+  ],
 });
 require('./main');
